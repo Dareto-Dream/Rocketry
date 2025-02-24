ede5848ad9d63e69ca70040b415702b88111f281
dareto-dream <dare.to.0ream@gmail.com>
2025-02-23 17:04:51 -1000

    space drone fix and datagen

---
+++ src/main/java/com/dareto/advancingrocketry/datagen/ModItemModelProvider.java
@@ -0,0 +1,24 @@
+package com.dareto.advancingrocketry.datagen;
+
+import com.dareto.advancingrocketry.AdvancingRocketry;
+import com.dareto.advancingrocketry.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, AdvancingRocketry.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.STEEL.get());
+        basicItem(ModItems.CHISEL.get());
+
+        basicItem(ModItems.ROCKET_FUEL.get());
+        basicItem(ModItems.CRUDE_OIL_BUCKET.get());
+
+        withExistingParent(ModItems.SPACEDRONE_SPAWN_EGG.getId().getPath(), mcLoc("item/template_spawn_egg"));
+    }
+}
